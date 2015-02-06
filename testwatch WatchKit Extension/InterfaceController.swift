3c30ba364343836f0822ba29bd0fc62f527de552
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-06 12:08:05 +0900

    iPhoneとやりとりするのを試してみた

--- testwatch WatchKit Extension/InterfaceController.swift
+++ testwatch WatchKit Extension/InterfaceController.swift
@@ -13,21 +13,43 @@ import Foundation
 class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var okButton: WKInterfaceButton!
+    
+    var count = 0
+    @IBOutlet weak var label: WKInterfaceLabel!
 
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        NSLog("%@ awakeWithContext", self)
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        label.setText("0")
+        NSLog("%@ will activate", self)
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
         super.didDeactivate()
+    }
+    
+    @IBAction func countUp() {
+        self.count += 1
+        label.setText("\(self.count)")
+    }
+    
+    @IBAction func sendCounter() {
+        //Send count to parent application
+        println("Watch \(self.count)")
+        WKInterfaceController.openParentApplication(["countValue": "\(self.count)"],
+            reply: {replyInfo, error in
+                println(replyInfo["fromApp"])
+        })
     }
     
     @IBAction func onOk(sender: AnyObject) {
--- testwatch/AppDelegate.swift
+++ testwatch/AppDelegate.swift
@@ -40,6 +40,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(application: UIApplication!, handleWatchKitExtensionRequest userInfo: [NSObject : AnyObject]!, reply: (([NSObject : AnyObject]!) -> Void)!) {
+        
+        var counterValue: String = userInfo["countValue"] as String!
+        
+        var appDel = UIApplication.sharedApplication().delegate as AppDelegate
+        var vc:ViewController? = appDel.window?.rootViewController as? ViewController
+        vc?.ulabel.text = counterValue
+        
+        println("Count: \(counterValue)")
+        
+        reply(["fromApp":"AppDelegate"])
+        
+    }
 
 
 }
